2d6f7b36d7cef79b5852d0f49516c34462d62d11
NotSaqlain <117275944+NotSaqlain@users.noreply.github.com>
2024-04-06 10:00:04 +0000

    add

---
+++ Coda.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+class Coda {
+private:
+    int size, start, stop;
+    int * v;
+
+public:
+    Coda(int size){
+        this->size = size;
+        v = new int [size];
+        start = stop = 0;
+    }
+
+    void enter(int val){
+        if(start = stop){
+            cout << "Coda piena" << endl;
+            return;
+        }
+        v[stop] = val;
+        stop++;
+    }
+
+    int exit(){
+        if (start >= stop){
+            cout << "Coda vuota" << endl;
+            return v[start++];
+        }
+        return -1;
+    }
+
+    void stampa(){
+        for (int i = start; i < stop; i++){
+            cout << v[i] << " ";
+        }
+        cout << "----------------------" << endl;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,47 +1,6 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
-class Coda {
-private:
-    int size, start, stop;
-    int * v;
-
-public:
-    Coda(int size){
-        this->size = size;
-        v = new int [size];
-        start = stop = 0;
-    }
-
-    void enter(int val){
-        if(start = stop){
-            cout << "Coda piena" << endl;
-            return;
-        }
-        v[stop] = val;
-        stop++;
-    }
-
-    int exit(){
-        if (start >= stop){
-            cout << "Coda vuota" << endl;
-            return v[start++];
-        }
-        return -1;
-    }
-
-    void stampa(){
-        for (int i = start; i < stop; i++){
-            cout << v[i] << " ";
-        }
-        cout << "----------------------" << endl;
-    }
-};
-
-
-
 /*
 class poste {
 private:
