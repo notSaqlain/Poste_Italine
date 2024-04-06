d3f2bfd77979c4a43a9875915371131182a87980
NotSaqlain <117275944+NotSaqlain@users.noreply.github.com>
2024-04-06 10:11:52 +0000

    fix

--- main.cpp
+++ main.cpp
@@ -17,5 +17,23 @@ int main() {
     c.enter(5);
     c.stampa();
 
+    cout << "Seleziona servizio: " << endl;
+    cout << "1. Spedizione" << endl;
+    cout << "2. Ricezione" << endl;
+    cout << "3. Finanzarie" << endl;
+    int servizio;
+    cin >> servizio;
+
+    if (servizio == 1){
+        poste p = poste("spedizione");
+    } else if (servizio == 2){
+        poste p = poste("ricezione");
+    } else if (servizio == 3){
+        poste p = poste("finanzarie");
+    } else {
+        cout << "Servizio non disponibile" << endl;
+    }
+
+    
     return 0;
 }
