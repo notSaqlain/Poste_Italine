bed83130d87a84dd3177e64cc3e07b29c89aec8f
NotSaqlain <iamnotsaqlain@gmail.com>
2024-04-06 23:22:00 +0200

    msg

--- poste.cpp
+++ poste.cpp
@@ -8,10 +8,10 @@ private:
     string S; // spedizione
     int R; //ricezione
     int F; //finanzarie
+    Coda c = Coda(1000);
     
 public:
     poste(string servizio){
-        Coda c = Coda(1000);
 
         if (servizio == "spedizione"){
             spedizione();
