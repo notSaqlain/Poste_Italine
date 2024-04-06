65053e400aeb42bd7b4ab6d98856a0e0cb8804f7
NotSaqlain <117275944+NotSaqlain@users.noreply.github.com>
2024-04-06 10:01:35 +0000

    add

---
+++ poste.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class poste {
+private:
+    string S; // spedizione
+    int R; //ricezione
+    int F; //finanzarie
+public:
+    poste(string servizio){
+        if (servizio == "spedizione"){
+            
+        } else if (servizio == "ricezione"){
+
+        } else if (servizio == "finanzarie"){
+
+        } else {
+            cout << "Servizio non disponibile" << endl;
+        }
+    }
+
+};
