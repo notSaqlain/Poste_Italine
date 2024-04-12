0b9430a7771efe5a0e85a347d4bb69df23e7d29c
NotSaqlain <iamnotsaqlain@gmail.com>
2024-04-12 22:02:58 +0200

    stop

--- main.cpp
+++ main.cpp
@@ -6,30 +6,38 @@
 using namespace std;
 
 int main() {
-    cout << "Benvenuto in Poste Italiane\n" << endl;
-    cout << "Seleziona servizio [1-3]: " << endl;
-    cout << "1. Spedizione (s)" << endl;
-    cout << "2. Ricezione (r)" << endl;
-    cout << "3. Finanzarie (f)" << "\n:";
-    int servizio;
-    cin >> servizio;
+    bool valido = true;
+    char risposta;
 
-    switch (servizio) {
-    case 1:
-        Poste("spedizione");
-        break;
-    case 2:
-        Poste("ricezione");
-        break;
-    case 3:
-        Poste("finanzarie");
-        break;
-    default:
-        cout << "Servizio non disponibile" << endl;
-        break;
+    while (valido) {
+        cout << "Benvenuto in Poste Italiane\n" << endl;
+        cout << "Seleziona servizio [1-3]: " << endl;
+        cout << "1. Spedizione (s)" << endl;
+        cout << "2. Ricezione (r)" << endl;
+        cout << "3. Finanzarie (f)" << "\n:";
+        int scelta;
+        cin >> scelta;
+
+        switch (scelta) {
+        case 1:
+            Poste("spedizione");
+            break;
+        case 2:
+            Poste("ricezione");
+            break;
+        case 3:
+            Poste("finanzarie");
+            break;
+        default:
+            cout << "Servizio non disponibile" << endl;
+            valido = false;
+            break;
+        }
+        cout << "Vuoi continuare? [s/n]: ";
+        cin >> risposta;
+        if (risposta == 'n') {
+            valido = false;
+        }
     }
-
-    
-
     return 0;
 }
